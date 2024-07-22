23ca0059d3ead110189c763ede25a067500e2626
Spone <spone185@gmail.com>
2024-07-22 13:07:22 +0300

    Json parsing added

--- Classes/Items/Chest.py
+++ Classes/Items/Chest.py
@@ -1,9 +1,10 @@
 import pygame as pg
 from gameSettings import *
 from Classes.Items.Items import *
+from Classes.Items.jsonImport import *
 
 class Chest():
     def __init__(self, game, pos):
         self.game = game
         self.pos = pos
-        self.items = {}
+        self.items = JsonImport(self.game, path.join(GAME_FOLDER, 'JsonData/items.json'))
--- Classes/Items/jsonImport.py
+++ Classes/Items/jsonImport.py
@@ -2,15 +2,31 @@ import pygame as pg
 from gameSettings import *
 import json
 
-class Import():
+class JsonImport():
     def __init__(self, game, file):
         self.game = game
 
         # Opening JSON file
         self.file = open(file)
+        self.loadFile()
 
     def loadFile(self):
-        data = json.load(self.file)
-
+        self.data = json.load(self.file)
         self.file.close()
+
+    # parse 'poisons.mana.small.mp'
+    def parseByPath(self, path, tempData = None):
+        
+        if len(path) == 0:
+            return tempData 
+        
+        if not tempData:
+            tempData = self.data
+        
+        path = path.split('.')
+        currentInd = path[0]
+        del path[0]
+
+        path = '.'.join(path)
+        return self.parseByPath(path, tempData[currentInd])
         
